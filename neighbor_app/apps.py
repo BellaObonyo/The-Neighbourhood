418a3d2fe157e1ff4979a6641b231973a0bca02a
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-25 22:39:50 +0300

    uppdated apps.py file

--- neighbor_app/apps.py
+++ neighbor_app/apps.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
-class SorroundingConfig(AppConfig):
-    name = 'sorrounding'
+class NeighborAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'neighbor_app'
