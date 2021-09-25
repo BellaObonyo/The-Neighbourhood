69b9c266a548c9fd3f41df24504538a3a67703ee
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-25 22:55:02 +0300

    uppdated tests.py file

--- neighbor_app/models.py
+++ neighbor_app/models.py
@@ -1,6 +1,102 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE, SET_NULL
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+  first_name = models.CharField(max_length=100, blank=True)
+  last_name = models.CharField(max_length=100, blank=True)
+  email = models.EmailField(max_length=150)
+  signup_confirmation = models.BooleanField(default=False)
+  bio =models.TextField(null=True)
+  profile_picture =CloudinaryField('image')
+  neighborhood = models.ForeignKey('NeighborHood', on_delete=SET_NULL,null=True, related_name='people', blank=True)
+  location =models.CharField(max_length=60,blank=True,null=True)
+
+  def __str__(self):
+    return self.user.username
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+  if created:
+      Profile.objects.create(user=instance)
+  instance.profile.save()
+  
+
+class NeighborHood(models.Model):
+  name = models.CharField(max_length=60)
+  location = models.CharField(max_length=60)
+  admin = models.ForeignKey(Profile,on_delete=CASCADE,related_name='administrator')
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  population = models.IntegerField(null=True,blank = True)
+  police_contact = models.IntegerField(null=True,blank = True)
+  hospital_contact = models.IntegerField(null=True,blank = True)
+  image = CloudinaryField('image')
+
+  def create_neighborhood(self):
+    self.save()
+
+  def delete_neighborhood(self):
+    self.delete()
+
+  @classmethod
+  def find_neighborhood(cls, neighborhood_id):
+    return cls.objects.filter(id=neighborhood_id)
+  
+  def __str__(self):
+    return self.name
+
+
+class Post(models.Model):
+  title = models.CharField(max_length=144)
+  post = models.TextField()
+  image = CloudinaryField('image')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User,on_delete=CASCADE,related_name='poster')
+  neighborhood = models.ForeignKey(NeighborHood,on_delete=CASCADE,related_name='neighborhood_post')
+
+  def save_post(self):
+    self.save()
+
+  def delete_post(self):
+    self.delete()
+
+  @classmethod
+  def show_posts(cls):
+    posts = cls.objects.all()
+    return posts
+
+  def __str__(self):
+    return self.title
+
+
+class Business(models.Model):
+  name =models.CharField(max_length=60)
+  description = models.TextField()
+  image = CloudinaryField('image')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  neighborhood = models.ForeignKey(NeighborHood,on_delete=CASCADE,related_name='business')
+  user = models.ForeignKey(User,on_delete=CASCADE)
+  email = models.EmailField()
+
+  def create_business(self):
+    self.save()
+
+  def delete_business(self):
+    self.delete()
+
+  @classmethod
+  def search_businesses(cls, business):
+    return cls.objects.filter(name__icontains=business).all()
+
+  def __str__(self):
+    return self.name
--- neighbor_app/tests.py
+++ neighbor_app/tests.py
@@ -1,6 +1,79 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.test import TestCase
-
 # Create your tests here.
+from neighbor_app.views import neighborhood
+from django.test import TestCase
+from .models import Business,Profile,NeighborHood,Post
+from django.contrib.auth.models import User
+
+
+class BusinessTest(TestCase):
+  def setUp(self):
+    self.user = User.objects.create(id=1, username='obonyo')
+    self.neighborhood = NeighborHood.objects.create(id=1, name='home')
+    self.busines = Business.objects.create(id=1, name='Moringa business', description='Moringa business description',image='https://cloudinary url',created_at='2021,6,26',updated_at='2021,6,26', neighborhood=self.neighborhood,user=self.user,email='nick@gmail.com')
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.busines, Business))
+
+  def test_create_business(self):
+    self.busines.create_business()
+    business = Business.objects.all()
+    self.assertTrue(len(business) > 0)
+
+  def test_get_business(self, id):
+    self.business.save()
+    business = Business.get_businesss(post_id=id)
+    self.assertTrue(len(business) == 1)
+
+
+class TestProfile(TestCase):
+  def setUp(self):
+    self.user = User(id=1, username='obonyo', password='akoth1998')
+    self.user.save()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.user, User))
+
+  def test_save_user(self):
+    self.user.save()
+
+  def test_delete_user(self):
+    self.user.delete()
+
+class PostTest(TestCase):
+  def setUp(self):
+    self.user = User.objects.create(id=1, username='obonyo')
+    self.neighborhood = NeighborHood.objects.create(id=1, name='home')
+    self.post = Post.objects.create(id=1, title='Moringa post', post='Moringa post description',image='https://cloudinary url',created_at='2021,6,26',updated_at='2021,6,26',user=self.user,neighborhood=self.neighborhood)
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.post, Post))
+
+  def test_display_posts(self):
+    self.post.save()
+    posts = Post.all_posts()
+    self.assertTrue(len(posts) > 0)
+
+  def test_save_post(self):
+    self.post.save_post()
+    post = Post.objects.all()
+    self.assertTrue(len(post) > 0)
+
+  def test_delete_post(self):
+    self.post.delete_post()
+    post = Post.search_post('random_post')
+    self.assertTrue(len(post) < 1)
+
+class NeighborhoodTest(TestCase):
+  def setUp(self):
+    self.user = User.objects.create(id=1, username='obonyo')
+    self.neighborhood = NeighborHood.objects.create(id=1, name='Moringa business', description='Moringa business description',image='https://cloudinary url',created_at='2021,6,26',updated_at='2021,6,26', neighborhood=self.neighborhood,user=self.user,email='nick@gmail.com')
+
+  def test_create_neighborhood(self):
+    self.neighborhood.create_neighborhood()
+    neighborhood = NeighborHood.objects.all()
+    self.assertTrue(len(neighborhood) > 0)
+
+  def test_get_neighborhood(self, id):
+    self.neighborhood.save()
+    neighborhood = NeighborHood.get_neighborhood(neighborhood_id=id)
+    self.assertTrue(len(neighborhood) == 1)
