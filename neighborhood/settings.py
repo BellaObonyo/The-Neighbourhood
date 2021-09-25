11255a0ffef185d36246f61690fd00579c73d5c6
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-25 22:36:32 +0300

    updated the projects urls.py file

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 # Email configurations remember to install python-decouple
 EMAIL_USE_TLS=True
 EMAIL_HOST=''
-EMAIL_PORT=
+EMAIL_PORT=587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
