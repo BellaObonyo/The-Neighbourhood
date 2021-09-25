ed04cb668bfab3ef7bc117561dc736ba398d6da9
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-25 22:39:10 +0300

    uppdated admin.py file

--- neighbor_app/admin.py
+++ neighbor_app/admin.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Business, NeighborHood, Post, Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(NeighborHood)
