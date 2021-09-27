39546b0d01062f07ea6bcfaf043e5d7d7b3548c7
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-27 21:17:23 +0300

    edited .env file

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -17,7 +17,7 @@ import cloudinary.api
 
 cloudinary.config(
     cloud_name =config('CLOUD_NAME'),
-    api_key=config('CLOUD_API_KEY'), 
+    api_key=config('API_KEY'), 
     api_secret=config('API_SECRET'),
     secure = config('SECURE'),
 )
